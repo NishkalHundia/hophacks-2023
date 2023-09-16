d1a0285ba0fe27c394c53f15488f1ee7fa62d444
NishkalHundia <nishkalnhundia@gmail.com>
2023-09-16 02:00:32 -0400

    added manual check and drug id

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
-import flask
+from flask import Flask, redirect, request
 import matplotlib.pyplot as plt
 import keras_ocr
 from drug_named_entity_recognition import find_drugs
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 pipeline = keras_ocr.pipeline.Pipeline()
 
-@app.route('/check', methods = ['GET', 'POST']) 
-def index():
-    if flask.request.method == 'POST':
-        # Get the file from post request
-        image = flask.request.files.get('medicineImage')
-        prediction_groups = pipeline.recognize(image)
-        s = []
-        for predictions in prediction_groups:
-            for x in predictions:
-                s.append(x[0]) 
+@app.route('/scan_check', methods = ['POST']) 
+def scan_check():
+    image = flask.request.files.get('medicineImage')
+    prediction_groups = pipeline.recognize(image)
+    s = []
+    for predictions in prediction_groups:
+        for x in predictions:
+            s.append(x[0]) 
 
+    try:
         return find_drugs(s, is_ignore_case=True)[0][0]['name']
+    except:
+        return "No drug found"
 
-    return 0
+@app.route('/manual_check', methods = ['POST'])
+def manual_check():
+    drug = []
+    drug.append(request.form.get('drug'))
+    try:
+        return find_drugs(drug, is_ignore_case=True)[0][0]['name']
+    except:
+        return "No drug found"
+    
+@app.route('/drug_id', methods = ['POST'])
+def drug_id():
+    drug = request.form.get('drug')
+    return redirect('https://rxnav.nlm.nih.gov/REST/rxcui.json?name=' + drug + '&search=1')
---
+++ name_identity_rec.py
@@ -0,0 +1,3 @@
+from drug_named_entity_recognition import find_drugs
+
+print(find_drugs("Hydrocodone is also a good drug.".split(" ")))
