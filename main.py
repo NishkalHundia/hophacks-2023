ca26a94d0c44c90ee786b326c29ad6ca9ed094d2
Loldude0 <aryan.tajne@gmail.com>
2023-09-16 01:21:09 -0400

    added drugs

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import flask
 import matplotlib.pyplot as plt
 import keras_ocr
+from drug_named_entity_recognition import find_drugs
 
 app = flask.Flask(__name__)
 pipeline = keras_ocr.pipeline.Pipeline()
@@ -11,10 +12,10 @@ def index():
         # Get the file from post request
         image = flask.request.files.get('medicineImage')
         prediction_groups = pipeline.recognize(image)
-        for image, predictions in zip(image, prediction_groups):
-            keras_ocr.tools.drawAnnotations(image=image, predictions=predictions)
-            print(predictions)
-            print("-----END-----")
+        s = []
+        for predictions in prediction_groups:
+            for x in predictions:
+                s.append(x[0])     
+        return find_drugs(s, is_ignore_case=True)[0][0]['name']
 
-        
-    return None
+    return 0
