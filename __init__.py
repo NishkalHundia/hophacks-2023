48864779fa263ee2c9d4473162889dd8ccdfeca5
Loldude0 <aryan.tajne@gmail.com>
2023-09-16 15:39:59 +0000

    smol changes

--- __init__.py
+++ __init__.py
@@ -56,97 +56,95 @@ class prescription(UserMixin, db.Model):
     drug_time = db.Column(db.String(16), nullable=False)
     expiry = db.Column(db.DateTime, nullable=False)
 
-def create_app():
+#Le login stuff
+@login_manager.user_loader
+def load_user(user_id):
+    return usertable.query.get(int(user_id))
 
-    #Le login stuff
-    @login_manager.user_loader
-    def load_user(user_id):
-        return usertable.query.get(int(user_id))
+# POST for registering
+@app.route("/register", methods=["POST"])
+def register():
+    name = request.args.get("name")
+    password = request.args.get("password")
+    first_name = request.args.get("first_name")
+    last_name = request.args.get("last_name")
+    height = request.args.get("height")
+    weight = request.args.get("weight")
+    date_of_birth = request.args.get("date_of_birth")
+    nurse_id = request.args.get("nurse_id")
+    doctor_id = request.args.get("doctor_id")
+    user = usertable(name=name)
+    db.session.add(user)
+    db.session.commit()
+    user = usertable.query.filter_by(name=name).first()
+    user_id = user.user_id
+    userpwd = userpwd(user_id=user_id, password=password)
+    userinfo = userinfo(user_id=user_id, first_name=first_name, last_name=last_name, height=height, weight=weight, date_of_birth=date_of_birth)
+    userstaff = userstaff(user_id=user_id, nurse_id=nurse_id, doctor_id=doctor_id)
+    db.session.add(userpwd)
+    db.session.add(userinfo)
+    db.session.add(userstaff)
+    db.session.commit()
+    return redirect("/login")
 
-    # POST for registering
-    @app.route("/register", methods=["POST"])
-    def register():
-        name = request.args.get("name")
-        password = request.args.get("password")
-        first_name = request.args.get("first_name")
-        last_name = request.args.get("last_name")
-        height = request.args.get("height")
-        weight = request.args.get("weight")
-        date_of_birth = request.args.get("date_of_birth")
-        nurse_id = request.args.get("nurse_id")
-        doctor_id = request.args.get("doctor_id")
-        user = usertable(name=name)
-        db.session.add(user)
-        db.session.commit()
-        user = usertable.query.filter_by(name=name).first()
-        user_id = user.user_id
-        userpwd = userpwd(user_id=user_id, password=password)
-        userinfo = userinfo(user_id=user_id, first_name=first_name, last_name=last_name, height=height, weight=weight, date_of_birth=date_of_birth)
-        userstaff = userstaff(user_id=user_id, nurse_id=nurse_id, doctor_id=doctor_id)
-        db.session.add(userpwd)
-        db.session.add(userinfo)
-        db.session.add(userstaff)
-        db.session.commit()
-        return redirect("/login")
-    
-    # POST for logging in
-    @app.route("/login", methods=["POST"])
-    def login():
-        name = request.args.get("name")
-        password = request.args.get("password")
-        user = usertable.query.filter_by(name=name).first()
-        if user is None:
-            return "User does not exist"
-        userpwd = userpwd.query.filter_by(user_id=user.user_id).first()
-        if userpwd.password != password:
-            return "Incorrect password"
-        login_user(user)
-        return str(user.user_id)
+# POST for logging in
+@app.route("/login", methods=["POST"])
+def login():
+    name = request.args.get("name")
+    password = request.args.get("password")
+    user = usertable.query.filter_by(name=name).first()
+    if user is None:
+        return "User does not exist"
+    userpwd = userpwd.query.filter_by(user_id=user.user_id).first()
+    if userpwd.password != password:
+        return "Incorrect password"
+    login_user(user)
+    return str(user.user_id)
 
-    # POST for image recognition
-    @app.route("/scan_check", methods=["POST"])
-    def scan_check():
-        image = Flask.request.files.get("medicineImage", "")
-        prediction_groups = pipeline.recognize(image)
-        s = []
+# POST for image recognition
+@app.route("/scan_check", methods=["POST"])
+def scan_check():
+    image = Flask.request.files.get("medicineImage", "")
+    prediction_groups = pipeline.recognize(image)
+    s = []
 
-        for predictions in prediction_groups:
-            for x in predictions:
-                s.append(x[0])
+    for predictions in prediction_groups:
+        for x in predictions:
+            s.append(x[0])
 
-        try:
-            return find_drugs(s, is_ignore_case=True)[0][0]["name"]
-        except:
-            return "No drug found"
+    try:
+        return find_drugs(s, is_ignore_case=True)[0][0]["name"]
+    except:
+        return "No drug found"
 
-    # POST for manual input
-    @app.route("/manual_check", methods=["POST"])
-    def manual_check():
+# POST for manual input
+@app.route("/manual_check", methods=["POST"])
+def manual_check():
 
-        drug = []
-        drug.append(request.args.get("drug"))
+    drug = []
+    drug.append(request.args.get("drug"))
 
-        try:
-            return find_drugs(drug, is_ignore_case=True)[0][0]["name"]
-        except:
-            return "No drug found"
+    try:
+        return find_drugs(drug, is_ignore_case=True)[0][0]["name"]
+    except:
+        return "No drug found"
 
-    @app.route("/check_compatibility", methods=["GET"])
-    def check_compatibility():
-        drug = request.args.get("drug")
-        #TODO: I fucking dont know how what why the fuck are we using flask bro
+@app.route("/check_compatibility", methods=["GET"])
+def check_compatibility():
+    drug = request.args.get("drug")
+    #TODO: I fucking dont know how what why the fuck are we using flask bro
 
-    @app.route("/add", methods=["POST"])
-    def add():
-        drug = request.args.get("drug")
+@app.route("/add", methods=["POST"])
+def add():
+    drug = request.args.get("drug")
 
-    # POST for getting the drug id, idk why is this here, seems useless to me
-    @app.route("/drug_id", methods=["POST"])
-    def drug_id():
+# POST for getting the drug id, idk why is this here, seems useless to me
+@app.route("/drug_id", methods=["POST"])
+def drug_id():
 
-        drug = request.args.get("drug")
+    drug = request.args.get("drug")
 
-        #return the rx number
-        return requests.get("https://rxnav.nlm.nih.gov/REST/rxcui.json?name=" + drug + "&search=1").json()['idGroup']['rxnormId'][0]
+    #return the rx number
+    return requests.get("https://rxnav.nlm.nih.gov/REST/rxcui.json?name=" + drug + "&search=1").json()['idGroup']['rxnormId'][0]
 
-    app.run(debug=True)
+app.run(debug=False)
