ee14aa72dfb05e7f4d24b27120000a16dded4bfe
Loldude0 <aryan.tajne@gmail.com>
2023-09-16 00:45:33 -0400

    something

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import flask
+import matplotlib.pyplot as plt
+import keras_ocr
 
 app = flask.Flask(__name__)
+pipeline = keras_ocr.pipeline.Pipeline()
 
 @app.route('/check', methods = ['GET', 'POST']) 
 def index():
     if flask.request.method == 'POST':
         # Get the file from post request
         image = flask.request.files.get('medicineImage')
+        prediction_groups = pipeline.recognize(image)
+        for image, predictions in zip(image, prediction_groups):
+            keras_ocr.tools.drawAnnotations(image=image, predictions=predictions)
+            print(predictions)
+            print("-----END-----")
 
         
     return None
